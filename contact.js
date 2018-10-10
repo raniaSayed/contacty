b6e54e870deff46fa8f6d5416c80d3d3faf004df
raniaSayed <raniasayed313@gmail.com>
2018-10-10 18:39:22 +0200

    get parameter values by inquirer

--- contact.js
+++ contact.js
@@ -2,23 +2,58 @@
 const program = require("commander");
 const {addContact , getContact} = require("./contacts_model");
 
+const { prompt } = require('inquirer');
+const questions = [
+    {
+        type:'input',
+        name: 'firstname',
+        message: 'Enter firstname... '
+    },
+
+    {
+        type: 'input',
+        name: 'lastname',
+        message: "Enter lastname ..."
+    },
+
+    {
+        type: 'input',
+        name: 'phone',
+        message: "Enter phone number ..."
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter email address ..."
+    }
+];
+
+const searchName = [{
+    type:'input',
+    name: 'name',
+    message: "Enter first or last name"
+}];
+
 program
     .version("0.0.1")
     .description("Contact Management System")
 
 program 
-    .command("new <firstname> <lastname> <phone> <email>")
+    .command("new")
     .alias("n")
     .description("Add new Contact")
-    .action((firstname,lastname,phone,email) => {
-        addContact({firstname,lastname,phone,email})
-    });
-
+    .action(()=> {
+        prompt(questions).then(answers =>
+            addContact(answers));
+        });
 
 program
-    .command("list <name>")
+    .command("list")
     .alias("l")
     .description(" List Contacts ")
-    .action(name => getContact(name));
-
+    .action(()=>{
+        prompt(searchName).then(answers =>
+          getContact(answers.name));
+        });
 program.parse(process.argv);
